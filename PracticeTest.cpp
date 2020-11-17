59221c46cbf4e411e1c2b4233fb6a8f33143a5e6
Adam Garcia <agarcia318@mail.csuchico.edu>
2020-11-17 00:02:47 -0800

    added tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -17,6 +17,51 @@ class PracticeTest : public ::testing::Test
 TEST(PracticeTest, is_simple_palindrome)
 {
     Practice obj;
-    bool actual = obj.isPalindrome("Not a palindrome");
+    bool actual = obj.isPalindrome("racecar");
+    ASSERT_TRUE(actual);
+
+}
+
+TEST(PracticeTest, is_non_character)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("$!^");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_blank_entry)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_mixed_value)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("ss^!^ss");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_easy_sort)
+{
+    int first = 1;
+    int second = 2;
+    int third = 3;
+    Practice obj;
+    bool actual = obj.sortDescending(first, second, third);
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_complex_sort)
+{
+    int first = 10;
+    int second = 1;
+    int third = 13;
+
+    Practice obj;
+    bool actual = obj.sortDescending(first, second, third);
+    ASSERT_TRUE(actual);
+}
+
+
